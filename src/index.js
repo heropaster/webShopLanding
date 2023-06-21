f38b6e538044305cb0f9b50f114d1d55e62b52f6
Дмитрий Ридель <ridel@pixlpark.local>
2023-06-21 17:19:33 +0700

    small changes at index responsive layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const searchBtn = document.querySelector('.btn__search');
 const topInput = document.querySelector('.top__input')
 $(searchBtn).on('click', function() {
-    $(this).toggleClass('active');
-    $(topInput).toggleClass('shown')
+    // $(this).toggleClass('active');
+    // $(topInput).toggleClass('mobile__hidden');
+    // $(topInput).toggleClass('mobile__shown');
+    // topInput.style.opacity = 1;
+    if ($(topInput).css('opacity') === '0') {
+        console.log(0)
+        $(topInput).animate('opacity', 1) ;
+    }else {
+        console.log(1)
+        $(topInput).animate('opacity', 0);
+    }
+    
 })
