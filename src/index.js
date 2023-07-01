cfdaee84ea9fe7d93e57bb5032f1c4d1b1f20a40
Heropaster <heropasta@mail.ru>
2023-07-01 15:59:26 +0700

    fixes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-const searchBtn = document.querySelector('.btn__search');
-const topInput = document.querySelector('.top__input')
-$(searchBtn).on('click', function() {
-    // $(this).toggleClass('active');
-    // $(topInput).toggleClass('mobile__hidden');
-    // $(topInput).toggleClass('mobile__shown');
-    // topInput.style.opacity = 1;
-    if ($(topInput).css('opacity') === '0') {
-        console.log(0)
-        $(topInput).animate('opacity', 1) ;
-    }else {
-        console.log(1)
-        $(topInput).animate('opacity', 0);
-    }
-    
-})
+const searchBtn = document.querySelector(".btn__search");
+const topInput = document.querySelector(".top__input");
+$(searchBtn).on("click", function () {
+  $(this).toggleClass("active");
+  $(topInput).toggleClass("mobile__hidden");
+  $(topInput).toggleClass("mobile__shown");
+  topInput.style.opacity = 1;
+  if ($(topInput).css("opacity") === "0") {
+    console.log(0);
+    $(topInput).animate("opacity", 1);
+  } else {
+    console.log(1);
+    $(topInput).animate("opacity", 0);
+  }
+});
